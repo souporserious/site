04a89a3ac66d9704a3d1a73e9591468f1ac0176f
souporserious <ftntravis@gmail.com>
2021-03-19 20:35:58 -0700

    upgrade dependencies

--- src/pages/drafts.js
+++ src/pages/drafts.js
@@ -4,35 +4,37 @@ import { graphql, Link } from 'gatsby'
 
 import { Layout } from '../components/Layout'
 
-export default ({ data }) => (
-  <Layout
-    noIndex
-    css={{
-      display: 'grid',
-      gridGap: '4em',
-      padding: '6em 2em',
-    }}
-  >
-    <h1>Drafts</h1>
-    <div
+export default function Drafts({ data }) {
+  return (
+    <Layout
+      noIndex
       css={{
         display: 'grid',
-        gridAutoRows: 'max-content',
-        gridGap: '0.5em',
+        gridGap: '4em',
+        padding: '6em 2em',
       }}
     >
-      {data.posts.nodes.map(post => (
-        <Link
-          key={post.frontmatter.title}
-          to={post.fields.slug}
-          css={{ fontSize: '1.5em' }}
-        >
-          {post.frontmatter.title}
-        </Link>
-      ))}
-    </div>
-  </Layout>
-)
+      <h1>Drafts</h1>
+      <div
+        css={{
+          display: 'grid',
+          gridAutoRows: 'max-content',
+          gridGap: '0.5em',
+        }}
+      >
+        {data.posts.nodes.map((post) => (
+          <Link
+            key={post.frontmatter.title}
+            to={post.fields.slug}
+            css={{ fontSize: '1.5em' }}
+          >
+            {post.frontmatter.title}
+          </Link>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,47 +4,49 @@ import { graphql, Link } from 'gatsby'
 
 import { Layout } from '../components/Layout'
 
-export default ({ data }) => (
-  <Layout
-    css={{
-      display: 'grid',
-      gridGap: '4em',
-      padding: '6em 2em',
-    }}
-  >
-    <div
-      css={{
-        alignSelf: 'center',
-        display: 'grid',
-        gridAutoRows: 'max-content',
-        gridGap: '0.5em',
-      }}
-    >
-      <h1>Travis Arnold</h1>
-      <h2 css={{ fontWeight: 300, color: 'rgb(173, 219, 103)' }}>
-        Designer / Developer
-      </h2>
-    </div>
-    <div
+export default function Index({ data }) {
+  return (
+    <Layout
       css={{
         display: 'grid',
-        gridAutoRows: 'max-content',
-        gridGap: '0.5em',
+        gridGap: '4em',
+        padding: '6em 2em',
       }}
     >
-      <h3>Recent Posts</h3>
-      {data.posts.nodes.map(post => (
-        <Link
-          key={post.frontmatter.title}
-          to={post.fields.slug}
-          css={{ fontSize: '1.5em' }}
-        >
-          {post.frontmatter.title}
-        </Link>
-      ))}
-    </div>
-  </Layout>
-)
+      <div
+        css={{
+          alignSelf: 'center',
+          display: 'grid',
+          gridAutoRows: 'max-content',
+          gridGap: '0.5em',
+        }}
+      >
+        <h1>Travis Arnold</h1>
+        <h2 css={{ fontWeight: 300, color: 'rgb(173, 219, 103)' }}>
+          Designer / Developer
+        </h2>
+      </div>
+      <div
+        css={{
+          display: 'grid',
+          gridAutoRows: 'max-content',
+          gridGap: '0.5em',
+        }}
+      >
+        <h3>Recent Posts</h3>
+        {data.posts.nodes.map((post) => (
+          <Link
+            key={post.frontmatter.title}
+            to={post.fields.slug}
+            css={{ fontSize: '1.5em' }}
+          >
+            {post.frontmatter.title}
+          </Link>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
